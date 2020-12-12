9e6a0f1e832aa7830e37b187e063598e6e8480c3
JecaTatu <gas5@cin.ufpe.br>
2020-12-12 14:32:51 -0300

    added material angular to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MaterialModule } from './material.module';
 import { AuthModule } from './auth/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,6 +6,7 @@ import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,13 @@ import { AppComponent } from './app.component';
     BrowserModule,
     ToastrModule.forRoot(),
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
+  exports:[
+    MaterialModule
+  ]
   providers: [],
   bootstrap: [AppComponent]
 })
